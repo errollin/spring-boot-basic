b13eb5dbf36030e1840c703f3d6a623e47492b16
Errol Lin <74441963+errollin@users.noreply.github.com>
2025-04-29 16:48:31 +0800

    Add test to verify handler autowiring

--- src/test/java/com/errol/springbootbasic/SpringBootBasicApplicationTests.java
+++ src/test/java/com/errol/springbootbasic/SpringBootBasicApplicationTests.java
@@ -1,10 +1,20 @@
 package com.errol.springbootbasic;
 
+import com.errol.springbootbasic.component.MyHandler;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootBasicApplicationTests {
+
+    @Autowired
+    private MyHandler myHandler;
+
+    @Test
+    void testAutowireOfHandler() {
+        System.out.println(this.myHandler.sayHello());
+    }
 
 	@Test
 	void contextLoads() {
