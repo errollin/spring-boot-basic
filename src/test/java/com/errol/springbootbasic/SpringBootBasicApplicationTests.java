d49e1b26871e63a3394ff8684d05b04d2c4a31b3
Errol Lin <74441963+errollin@users.noreply.github.com>
2025-04-29 16:49:13 +0800

    Add test to get handler bean from application context

--- src/test/java/com/errol/springbootbasic/SpringBootBasicApplicationTests.java
+++ src/test/java/com/errol/springbootbasic/SpringBootBasicApplicationTests.java
@@ -4,9 +4,13 @@ import com.errol.springbootbasic.component.MyHandler;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class SpringBootBasicApplicationTests {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Autowired
     private MyHandler myHandler;
@@ -14,6 +18,12 @@ class SpringBootBasicApplicationTests {
     @Test
     void testAutowireOfHandler() {
         System.out.println(this.myHandler.sayHello());
+    }
+
+    @Test
+    void testGetBeanFromContext() {
+        MyHandler handler = applicationContext.getBean(MyHandler.class);
+        System.out.println(handler.sayHello());
     }
 
 	@Test
